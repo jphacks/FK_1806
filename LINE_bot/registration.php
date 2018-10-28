cea6ce3973400b7152c68b46616d0a911175da5c
HallelujahStone <aaharuyana@gmail.com>
2018-10-28 13:46:39 +0900

    微調整

--- LINE_bot/registration.php
+++ LINE_bot/registration.php
@@ -84,7 +84,6 @@ print("Hello");
 function toEmoji($code){
   $bin = hex2bin(str_repeat('0', 8 - strlen($code)) . $code);
   $emoticon =  mb_convert_encoding($bin, 'UTF-8', 'UTF-32BE');
-
   return $emoticon;
 }
 
